17a4f4dc6e74ac8908dbcfde7c8b28fd602ffbc7
Vishal Zade <vishalzade75@gmail.com>
2024-06-09 21:20:34 +0530

    Template driven form setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-forms';
+
+  onSubmit(contactForm: any) {
+    console.log(contactForm.value);
+  }
 }
